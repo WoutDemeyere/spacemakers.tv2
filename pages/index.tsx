cb04de0b4e303c01302f41384a99f70c6c357e90
Wout Demeyere <wout.demeyere@qity.be>
2024-10-02 09:51:16 +0200

    rotate carousel on click

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import React, { useRef } from "react";
 
 import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
+import { SwiperOptions } from 'swiper/types';
+
 import { EffectFade, Autoplay } from "swiper/modules";
 import Slogan from "../components/Slogan";
 
@@ -13,56 +15,55 @@ const images: string[] = [
   "/images/landing/cotf.jpg",
   "/images/landing/ion.jpg",
   "/images/landing/bbr_opt_2.jpg",
-	"/images/landing/asa_moto.jpeg",
-	"/images/landing/ccd.jpg",
-	"/images/landing/obscuur.jpg",
-	"/images/landing/nye.jpg",
+  "/images/landing/asa_moto.jpeg",
+  "/images/landing/ccd.jpg",
+  "/images/landing/obscuur.jpg",
+  "/images/landing/nye.jpg",
 ];
 
 const Home: React.FC = () => {
-	const swiperRef = useRef<typeof Swiper | null>(null);
+  const swiperRef = useRef<any | null>(null);
 
-	// const handleImageClick = () => {
-	// 	if (swiperRef.current) {
-	// 		swiperRef.current.slideNext();
-	// 	}
-	// };
+  const handleImageClick = () => {
+    swiperRef.current?.slideNext();
+  };
 
-	return (
-		<React.Fragment>
-			<div className={styles.slogan_container}>
-				<Slogan />
-			</div>
+  return (
+    <React.Fragment>
+      <div className={styles.slogan_container}>
+        <Slogan />
+      </div>
 
-			<Swiper
-				loop={true}
-				slidesPerView={1}
-				modules={[EffectFade, Autoplay]}
-				effect="fade"
-				className={styles.container}
-				autoplay={{
-					delay: 5000,
-					disableOnInteraction: false,
-				}}
-				// onSwiper={(swiper) => {
-				// 	swiperRef.current = swiper;
-				// }}
-        >
-				{images.map((el, key) => {
-					return (
-						<SwiperSlide key={key}>
-							<img
-								src={el}
-								alt={el.split("/").pop()?.split(".")[0]}
-								className={styles.carouselImage}
-								// onClick={handleImageClick}
-							/>
-						</SwiperSlide>
-					);
-				})}
-			</Swiper>
-		</React.Fragment>
-	);
+      <Swiper
+        allowSlideNext
+        loop={true}
+        slidesPerView={1}
+        modules={[EffectFade, Autoplay]}
+        effect="fade"
+        className={styles.container}
+        autoplay={{
+          delay: 5000,
+          disableOnInteraction: false,
+        }}
+        onSwiper={(swiper) => {
+          swiperRef.current = swiper;
+        }}
+      >
+        {images.map((el, key) => {
+          return (
+            <SwiperSlide key={key}>
+              <img
+                src={el}
+                alt={el.split("/").pop()?.split(".")[0]}
+                className={styles.carouselImage}
+                onClick={() => handleImageClick()}
+              />
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </React.Fragment>
+  );
 };
 
 export default Home;
