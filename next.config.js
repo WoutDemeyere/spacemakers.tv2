4f38507818cdc6738673e252b169c2df6823d291
Wout Demeyere <wout.demeyere@qity.be>
2024-11-09 14:21:44 +0100

    prep for flyio

--- next.config.js
+++ next.config.js
@@ -1,11 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   trailingSlash: true,
-  distDir: 'build',
   output: 'standalone',
   reactStrictMode: true,
   images: {
-    domains: [ "storage.googleapis.com", "127.0.0.1", "admin.spacemakers.tv" ],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'storage.googleapis.com'
+      },
+      {
+        protocol: 'http',
+        hostname: '127.0.0.1'
+      },
+      {
+        protocol: 'https',
+        hostname: 'admin.spacemakers.tv'
+      }
+    ],
   },
 }
 
